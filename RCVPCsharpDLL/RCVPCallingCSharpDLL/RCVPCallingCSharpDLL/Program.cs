84ace023a01a20e35369af0d8a4c7719148ebdee
Navid-Derakhshandeh <111235264+Navid-Derakhshandeh@users.noreply.github.com>
2025-02-19 00:32:59 +0330

    RCVP C# DLL

---
+++ RCVPCsharpDLL/RCVPCallingCSharpDLL/RCVPCallingCSharpDLL/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RCVPCsharpDLL;
+namespace RCVPCallingCSharpDLL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter the Current Value : ");
+            double I = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please Enter the Voltage Value : ");
+            double V = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please Enter the Resistance Value : ");
+            double R = Double.Parse(Console.ReadLine());
+
+            double P = RCVPCSDLL.Power(V, I);
+            double C = RCVPCSDLL.Current(V, R);
+            double RE = RCVPCSDLL.Resistance(V, I);
+            double VO = RCVPCSDLL.Voltage(I, R);
+
+            Console.WriteLine("Power Equal By : {0}.", P);
+            Console.WriteLine("Current Equal By : {0}.", C);
+            Console.WriteLine("Resistance Equal By : {0}.", RE);
+            Console.WriteLine("Voltage Equal By : {0}.", VO);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RCVPCsharpDLL/RCVPCsharpDLL/RCVPCsharpDLL/RCVPCSDLL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCVPCsharpDLL
+{
+    public class RCVPCSDLL
+    {
+        
+		public static double Power(double V1, double I1)
+		{
+			return V1 * I1;
+		}
+		public static double Current(double V2, double R2)
+		{
+			return V2 / R2;
+		}
+		public static double Voltage(double I3, double R3)
+		{
+			return I3 * R3;
+		}
+		public static double Resistance(double V4, double I4)
+		{
+			return V4 / I4;
+		}
+	}
+}
